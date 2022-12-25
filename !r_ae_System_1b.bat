a889b1e68404f8e8eedec4b724b2f7bcf5c65330
sergey-frd <sergey.frd@gmail.com>
2022-12-25 21:53:10 +0200

    221225
    221225

--- !r_ae_System_1b.bat
+++ !r_ae_System_1b.bat
@@ -1,3 +1,4 @@
 c:
 cd c:\Program Files\Adobe\Adobe After Effects 2023\Support Files\
-AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_b_Raya\\sf_System_22_s5b.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_b_Raya\\sf_System_22_s5b.jsx"
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_b_Raya\\sf_System_22_s5_3.jsx"
